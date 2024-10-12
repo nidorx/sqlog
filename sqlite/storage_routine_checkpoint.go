83ba8b04f6dc8e78647e6406937f01e85d6a7079
Alex Rodin <contato@alexrodin.info>
2024-10-11 21:28:12 -0300

    fix data race

--- sqlite/storage_routine_checkpoint.go
+++ sqlite/storage_routine_checkpoint.go
@@ -14,10 +14,17 @@ func (s *storage) routineWalCheckpoint() {
 		select {
 
 		case <-tick.C:
+			var liveOpenDbs []*storageDb
+			s.mu.Lock()
 			for _, db := range s.liveDbs {
 				if db.isOpen() {
-					db.checkpoint(s.config.WalCheckpointMode)
+					liveOpenDbs = append(liveOpenDbs, db)
 				}
+			}
+			s.mu.Unlock()
+
+			for _, db := range liveOpenDbs {
+				db.checkpoint(s.config.WalCheckpointMode)
 			}
 
 			tick.Reset(d)
